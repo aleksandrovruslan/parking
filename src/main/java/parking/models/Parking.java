65c9048a6bc48d4be9b97e7a3d36c986e0c90eb2
Ruslan <aleksruslan@mail.ru>
2019-04-10 20:56:28 +0300

    add entity parking

--- src/main/java/parking/models/Parking.java
+++ src/main/java/parking/models/Parking.java
@@ -1,4 +1,25 @@
 package parking.models;
 
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Parking {
+
+    private Map<Integer, Car> cars;
+    private Queue<Ticket> tickets = new ConcurrentLinkedQueue<>();
+
+    public Parking(int parkingSize) {
+        cars = new ConcurrentHashMap<>(parkingSize);
+    }
+
+    public Map<Integer, Car> getCars() {
+        return cars;
+    }
+
+    public Queue<Ticket> getTickets() {
+        return tickets;
+    }
+
 }
